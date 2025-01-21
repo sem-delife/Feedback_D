99cb39f25e60be778612779736e5b9de7e898ea7
Sean Mueller <sem@delife.eu>
2025-01-21 13:34:57 +0100

    New: Identity Inital

---
+++ Feedback-Application/Areas/Identity/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Feedback_Application.Areas.Identity
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+
+}
--- Feedback-Application/Program.cs
+++ Feedback-Application/Program.cs
@@ -1,16 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Feedback_Application.Areas.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ConnectionString für die Datenbank
+var connectionString = builder.Configuration.GetConnectionString("FeedbackDb");
+
+// DbContext registrieren
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Identity registrieren (mit DefaultIdentity oder vollständigem Setup, nicht beides!)
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Razor Pages hinzufügen
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// HTTP-Request-Pipeline konfigurieren
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(); // Standard-HSTS-Konfiguration
 }
 
 app.UseHttpsRedirection();
@@ -18,8 +33,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // Identity-Authentifizierung aktivieren
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapRazorPages(); // Razor Pages aktivieren
 
 app.Run();
