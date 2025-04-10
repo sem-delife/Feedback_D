4f2c12b5b252d72e8d51e3de77c40abe153ca6f7
Sean Mueller <sem@delife.eu>
2025-04-10 14:09:11 +0200

    Final Release

---
+++ Feedback-Application/Pages/Relax.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Feedback_Application.Pages
+{
+    public class RelaxModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
